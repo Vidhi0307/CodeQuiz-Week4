58fe22db35fbee65d15e95e2207c32f797b294e9
Akhilesh Sharma <vidhi.sydney07@gmail.com>
2022-08-27 01:26:22 +1000

    changes to css and js for rendering quiz questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
 var timerCount;
 var startButton =document.querySelector( ".start-button");
-var quizContainer=document.querySelector("#quiz");
+var quizQuestions= document.getElementById('quizQuestions')
+var currentQuestion= document.getElementById('question')
+var buttonOptions= document.getElementById('answers')
 var timerElement =document.querySelector(".timer-count");
+var QuesIndex=0;
 
 var myQuestions = [
     {
@@ -11,7 +14,7 @@ var myQuestions = [
             2: "curly brackets",
             3: "quotes",
             4:  "parenthesis"
-    },correctAnswer: "1"
+    },correctAnswer: "commas"
     },{
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
         answers : {
@@ -19,7 +22,7 @@ var myQuestions = [
         2: "terminal/bash",
         3: "for loops",
         4: "console log"
-        },correctAnswer: "4"
+        },correctAnswer: "console log"
         },
     {
         question: "Commonly used datatypes do not include:",
@@ -28,7 +31,7 @@ var myQuestions = [
             2: "booleans",
             3: "alerts",
             4:  "numbers"
-    },correctAnswer: "3"
+    },correctAnswer: "alerts"
     },{
         question: "The condition in an if/else statement is enclosed within ________",
         answers : {
@@ -36,7 +39,7 @@ var myQuestions = [
             2: "curly brackets",
             3: "parenthesis",
             4: "square brackets"
-    },correctAnswer: "3"
+    },correctAnswer: "parenthesis"
     },{
         question: "Arrays in Javascript can be used to store ________",
         answers : {
@@ -44,7 +47,7 @@ var myQuestions = [
             2: "other arrays",
             3: "",
             4: "all of the above"
-    },correctAnswer: "4"
+    },correctAnswer: "all of the above"
     },]
 
 
@@ -61,18 +64,34 @@ function startQuiz(){
 
 function renderQuiz() {
     console.log(document.getElementById("quizQuestions"));
-
+    //hiding the start button
     document.getElementById('container').style.display = "none";
-    document.getElementById('quizQuestions').style.display = "block";
-    
 
-    var Test='test';
-   // startButton.hidden = true;
-   // quizContainer.innerHTML= '<p>Test</p>';
+   // displaying the quiz question window
+   quizQuestions.style.display = "block";
+
+    var currentAnswerOptions = myQuestions[QuesIndex].answers;
    
+ for (const property in currentAnswerOptions) {
+    var answerButton = document.createElement("button");
+    console.log(currentAnswerOptions[property]);
+   // answerButton.setAttribute("value", correctAnswer);
+   var option= currentAnswerOptions[property];
+   console.log(option);
+    answerButton.textContent = option;
+    currentQuestion.textContent =  myQuestions[QuesIndex].question;
+    buttonOptions.appendChild(answerButton);
 
+    answerButton.addEventListener("click",Iscorrect);
 }
 
+  }
+
+ function Iscorrect () {
+    console.log("called");
+if(this.textContent === myQuestions[QuesIndex].correctAnswer)
+{window.alert("Correct");}
+  }
 
 //The set Timer will start and stops the timer and triggers score()
 
